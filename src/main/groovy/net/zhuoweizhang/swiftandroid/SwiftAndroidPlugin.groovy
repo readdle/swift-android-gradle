eb899286a45977e21126daa7fac9c178e9db8f38
Anton Pogonets <apogonets@readdle.com>
2017-11-16 20:27:16 +0200

    Unify swift command naming

--- src/main/groovy/net/zhuoweizhang/swiftandroid/SwiftAndroidPlugin.groovy
+++ src/main/groovy/net/zhuoweizhang/swiftandroid/SwiftAndroidPlugin.groovy
@@ -15,52 +15,54 @@ class SwiftAndroidPlugin implements Plugin<Project> {
     void apply(Project project) {
         def extension = project.extensions.create('swift', SwiftAndroidPluginExtension)
 
-        Task generateSwiftTask = createGenerateSwiftTask(project)
-        Task compileSwiftTask = createCompileSwiftTask(project)
-        Task copySwiftStdlibTask = createCopyStdlibTask(project)
-        Task copySwiftTask = createCopyTask(project)
-        Task cleanSwift = createCleanTask(project)
-        createSwiftUpdateTask(project)
+        Task generateSwift = createGenerateSwiftTask(project)
 
-        copySwiftTask.dependsOn(compileSwiftTask, extension, copySwiftStdlibTask)
+        Task copySwiftStdlib = createCopyStdlibTask(project)
+        Task swiftBuild = createSwiftBuildTask(project)
+        Task copySwift = createCopyTask(project)
+        copySwift.dependsOn(swiftBuild, copySwiftStdlib)
+
+        Task swiftClean = createCleanTask(project)
+        createSwiftInstallTask(project)
+        createSwiftUpdateTask(project)
 
         project.afterEvaluate {
             // according to Protobuf gradle plugin, the Android variants are only available here
             // TODO: read those variants; we only support debug right now
 
             Task compileNdkTask = project.tasks.getByName("compileDebugNdk")
-            compileNdkTask.dependsOn(copySwiftTask)
+            compileNdkTask.dependsOn(copySwift)
 
             Task preBuildTask = project.tasks.getByName("preBuild")
-            preBuildTask.dependsOn(generateSwiftTask)
+            preBuildTask.dependsOn(generateSwift)
 
             if (extension.cleanEnabled) {
                 Task cleanTask = project.tasks.getByName("clean")
-                cleanTask.dependsOn(cleanSwift)
+                cleanTask.dependsOn(swiftClean)
             }
         }
     }
 
     private static Task createCleanTask(Project project) {
-        Task forceCleanSwift = project.task(type: Delete, "forceCleanSwift") {
+        Task forceClean = project.task(type: Delete, "swiftForceClean") {
             // I don't trust Swift Package Manager's --clean
             delete "src/main/swift/.build"
         }
 
-        Task swiftPackageClean = project.task(type: Exec, "swiftPackageClean") {
+        Task packageClean = project.task(type: Exec, "swiftPackageClean") {
             workingDir "src/main/swift"
             commandLine "swift", "package", "clean"
         }
 
-        Task metaTask = project.task("cleanSwift")
+        Task metaTask = project.task("swiftClean")
 
         project.afterEvaluate {
             def extension = project.extensions.getByType(SwiftAndroidPluginExtension)
 
-            Task swiftCleanTask = extension.usePackageClean ?
-                    swiftPackageClean : forceCleanSwift
+            Task impl = extension.usePackageClean ?
+                    forceClean : packageClean
 
-            metaTask.dependsOn(swiftCleanTask)
+            metaTask.dependsOn(impl)
         }
 
         return metaTask
@@ -85,9 +87,28 @@ class SwiftAndroidPlugin implements Plugin<Project> {
         }
     }
 
-    private static Task createCompileSwiftTask(Project project) {
-        return project.task(type: Exec, "compileSwift") {
+    private static Task createSwiftBuildTask(Project project) {
+        Task swiftBuild = project.task(type: Exec, "swiftBuild") {
             commandLine(getScriptRoot() + "swift-build.sh")
+            workingDir("src/main/swift")
+        }
+
+        addCompatibilityAlias(project, swiftBuild, "compileSwift")
+
+        return swiftBuild
+    }
+
+    private static Task addCompatibilityAlias(Project project, Task task, String alias) {
+        String originalName = task.name
+
+        return project.task(alias).dependsOn(task).doFirst {
+            print "${alias} is deprecated use ${ originalName}"
+        }
+    }
+
+    private static Task createSwiftInstallTask(Project project) {
+        return project.task(type: Exec, "swiftInstall") {
+            commandLine(getScriptRoot() + "invoke-external-build.sh")
             workingDir("src/main/swift")
         }
     }
